983d05cfb4519c841df6f8c7c1133e9c45e34e1f
Carlos Roberto Arias Arevalo <arias@spu.edu>
2022-02-28 11:27:18 -0800

    Made a main to test
    Tested ctor, dtor, add to top, write and size

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+#include "linked_list.h"
+
+using std::cout;
+using std::endl;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    LinkedList baby;
+    cout << "Output Empty List" << endl;
+    cout << "Size = " << baby.Size() << endl;
+    baby.Write(cout);
+    cout << endl;
+
+    baby.AddToTop(4);
+    baby.AddToTop(1);
+    baby.AddToTop(7);
+    baby.AddToTop(3);
+    baby.AddToTop(5);
+    cout << "Output List with Stuff" << endl;
+    cout << "Size = " << baby.Size() << endl; // 5
+    baby.Write(cout); // 5 3 7 1 4
+    cout << endl;
+
+
     return 0;
 }
